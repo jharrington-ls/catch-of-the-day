0bcce274703f214e952b43417e28007fe6be9776
John Harrington <jharrington@launchsquad.com>
2016-11-25 17:35:27 -0500

    End of Video 04

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 class StorePicker extends React.Component{
     render(){
+        //This is a normal Javascript comment
         return(
-            <form>
-            
+            <form className="store-selector">
+                {/* This is a JSX comment */}
+                <h2>Please Enter a Store Name</h2>
+                <input type="text" required placeholder="Store Name"/>
+                <button type="submit">Visit Store</button>
             </form>
         )
     }

14eb57b6741039e8a8205f78c5497aef0781756a
John Harrington <jharrington@launchsquad.com>
2016-11-27 15:59:26 -0500

    End of Video 11. Introduction to React events

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,13 +2,24 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component{
+    // constructor(){
+    //     super();
+    //     this.goToStore = this.goToStore.bind(this);
+    // }
+    goToStore(event){
+        event.preventDefault();
+        console.log("You changed the url");
+        //First get the store name from the input
+        console.log(this.storeInput.value);
+        //Second update the url with React Router
+    }
     render(){
         //This is a normal Javascript comment
         return(
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
                 {/* This is a JSX comment */}
                 <h2>Please Enter a Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
                 <button type="submit">Visit Store</button>
             </form>
         )

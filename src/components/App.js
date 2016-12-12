166f8f6e8cc7376ab443abb3d4cd39d026ec7cc6
John Harrington <jharrington@launchsquad.com>
2016-12-11 16:15:15 -0800

    End of Video 14. Added state via onClick on button

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component{
     constructor(){
         super();
         this.addFish = this.addFish.bind(this);
+        this.loadSamples = this.loadSamples.bind(this);
         // getInitialState
         this.state = {
             fishes: {},
@@ -22,6 +24,11 @@ class App extends React.Component{
         //Set State
         this.setState({fishes: fishes});
     }
+    loadSamples(){
+        this.setState({
+            fishes: sampleFishes
+        });
+    }
     render(){
         return(
             <div className="catch-of-the-day">
@@ -29,7 +36,7 @@ class App extends React.Component{
                     <Header tagline="Fresh seafood market"/>
                 </div>
                 <Order />
-                <Inventory addFish={this.addFish}/>
+                <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
             </div>
         )
     }
